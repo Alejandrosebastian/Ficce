59d81200971db2c4832e858bf1b2a25cf843ee4b
Sebastian Alejandro <kanfyslol@gmail.com>
2018-07-21 15:21:38 -0500

    ok

---
+++ FICCE/FICCE/Models/Evento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FICCE.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public int año { get; set; }
+        public string  mes { get; set; }
+
+
+    }
+}
---
+++ FICCE/FICCE/Models/Tipoempresa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FICCE.Models
+{
+    public class Tipoempresa
+    {
+        public int TipoempresaId { get; set; }
+        public string  Nombre { get; set; }
+        public string Detalle { get; set; }
+        
+    }
+}
