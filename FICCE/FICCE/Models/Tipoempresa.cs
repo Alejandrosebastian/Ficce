ac07b5189200ee2498880b3e8ff12ba5afaf1a28
Sebastian Alejandro <kanfyslol@gmail.com>
2018-07-30 12:25:06 -0500

    ok

--- FICCE/FICCE/Models/Tipoempresa.cs
+++ FICCE/FICCE/Models/Tipoempresa.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FICCE.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace FICCE.Models
 {
     public class Tipoempresa
     {
-       
+        private ApplicationDbContext context;
+
+        public Tipoempresa(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
 
         public int TipoempresaId { get; set; }
         public string  Nombre { get; set; }
         public string Detalle { get; set; }
-        
+
+        internal List<IdentityError> ModeloGuardatipoempres(string detalle, string nombre)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
